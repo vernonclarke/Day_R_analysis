e1575f2fd105b860fcec9410231dc93be949a5ad
Vernon <149691755+vernonclarke@users.noreply.github.com>
2023-11-22 15:27:18 -0600

    Update graphs and stats.R

--- graphs and stats.R
+++ graphs and stats.R
@@ -1,14 +1,3 @@
-#################################
-# requires R
-# download from https://cran.r-project.org/
-# As stated:
-# Note: the use of X11 (including tcltk) requires XQuartz (version 2.8.5 or later). 
-# Always re-install XQuartz when upgrading your macOS to a new major version. 
-
-# this code should pull data from csv files and analyse it/produce boxplots etc and recreate relevant parts of the figures
-# returns svg files that are then used to produce figures in Adobe Illustrator
-# Vernon Clarke : vernon.clarke@northwestern.edu
-
 #### MAIN CODE
 rm( list=ls(all=TRUE ) )
 load_required_packages <- function(packages){
